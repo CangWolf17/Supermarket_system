031b295a30d1d1385044cf69d557ae2c23426d10
CangWolf17 <s1694713403@163.com>
2023-12-22 09:25:11 +0800

    重构项目
    1.重构了整个项目的结构
    2.创建了源码文件夹src
    3.针对整个项目和src创建了CMakeLists.txt

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "iostream"
-
-using namespace  std;
-
-void marry_in(int n,int x[]){
-    for (int i=0;i<n;i++) cin>>x[i];
-}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by calfVong on 2023/12/22.
+//
+#include "iostream"
+
+using namespace std;
+
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
