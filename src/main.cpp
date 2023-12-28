f545e3f8ad4323d8910968af2870d56bbc8a0c7c
CangWolf17 <s1694713403@163.com>
2023-12-28 23:19:49 +0800

    更新主函数，初步实现方案内容
    1.更新gitignore，忽略了产生的exe文件
    2.由clion更新的cmake文件
    3.更新了main.cpp。
    3.1 引入了一些头文件。
    3.2 使用了枚举来表示不同权限对应的数字，使得代码可读性提高。
    3.3 初步实现了登录部分
    3.4 实现了打开goods.txt文件并读取其中的货物数据，存放到名为goods的vector数组中
    3.5 根据不同的用户权限创建不同的用户函数类，方便后续进行调用

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 //
 // Created by calfVong on 2023/12/22.
 //
-#include "iostream"
-#include "cstring"
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
 #include "head.h"
 
 using namespace std;
 
 // 主函数，保持为登陆界面模块
 int main(){
-    users user = login();
+    enum authority{
+        customer = 0, keeper, cashier, admin
+    };  // 枚举 使用户权限可视为单词
+
+    Display display;            // 实例化一个Display对象
+    Users user = login();       // 调用login函数实现登录，同时返回一个包含用户数据的users对象
+    Goods goodsFunc;    // 创建一个goodsfunc用于调用成员函数
+
+    vector<Goods> goods;            // 申请一个goods数组
+    goodsFunc.read(goods);       // 读取goods文件中的数据并存入数组
+
+    // 根据用户权限创建用于调用函数的对象（无法使用switch语句）
+    if(user.userLevel == admin)
+        Admin func;
+    else if(user.userLevel == cashier)
+        Cashier func;
+    else if(user.userLevel == keeper)
+        Keeper func;
+    else
+        Customer func;
+
+
     return 0;
 }
