bcd961d51484aca4df12e83e0ff39d9e03b27c66
CangWolf17 <s1694713403@163.com>
2023-12-28 23:10:15 +0800

    构建了四类用户对应的cpp框架
    创建了类对应的cpp，并写入了基本的权限对应的函数

---
+++ src/users/Admin.cpp
@@ -0,0 +1,19 @@
+//
+// Created by calfVong on 2023/12/28.
+//
+#include "iostream"
+
+#include "..\head.h"
+
+using namespace std;
+
+void Admin::users_edit() {
+
+
+}
+
+void Admin::users_create() {
+
+
+
+}
