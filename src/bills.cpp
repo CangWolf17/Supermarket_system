2eea3cddf5b935fde8a69e4af94f4a1adfdbef6f
CangWolf17 <s1694713403@163.com>
2024-01-05 08:39:29 +0800

    撤销了上一条修改 "完善了goods类，bills类的函数"
    This reverts commit b0bf68add504dddddd3d3db539213b6ce079eaa0.

--- src/bills.cpp
+++ src/bills.cpp
@@ -50,43 +50,17 @@ void Bills::save(vector<Bills> &bills) {
     txt_data.close();
 }
 
-void Bills::receipt(vector<Bills> &bills){
-    // 打印小票的逻辑
-    // 根据当前的销售记录，输出对应的销售详情，包括交易编码、时间、编号、商品名称、种类、单价、商品数量、总价、利润和计量单位等
-    cout << "小票内容：" << endl;
-    cout << "交易编码\t时间\t编号\t商品名称\t种类\t单价\t商品数量\t总价\t利润\t计量单位" << endl;
-    for (const auto& bill : bills) {
-        cout << bill.transactionID << "\t"; // 交易编码
-        cout << bill.time << "\t"; // 时间
-        cout << bill.id << "\t"; // 编号
-        cout << bill.name << "\t"; // 商品名称
-        cout << bill.species << "\t"; // 种类
-        cout << bill.sellPrice << "\t"; // 单价
-        cout << bill.quantity << "\t"; // 商品数量
-        cout << bill.sellPrice * bill.quantity << "\t"; // 总价
-        cout << bill.profit << "\t"; // 利润
-        cout << bill.measure; // 计量单位
-        cout << endl;
-    }
+void Bills::receipt(){
+
 }
 
-void Bills::data(vector<Bills> &bills) {
-    // 输出销售记录的逻辑
-    // 根据存储的销售记录，逐条输出记录的详细信息，包括交易ID、时间、商品名称、销售价格等
-    cout << "销售记录：" << endl;
-    for (const auto& bill : bills) {
-        cout << "交易ID：" << bill.transactionID << ", 时间：" << bill.time << ", 商品名称：" << bill.name
-             << ", 销售价格：" << bill.sellPrice << endl;
-    }
+void Bills::data() {
+
 }
 
-void Bills::newLog(vector<Bills> &bills){
-// 新增销售记录的逻辑
-    // 根据输入的商品信息，创建一个新的销售记录，并添加到存储的销售记录中
-    Bills newBill;
-    cout << "请输入交易ID、时间、商品名称、种类、销售价格、数量、进价、售价和备注：" << endl;
-    cin >> newBill.transactionID >> newBill.time >> newBill.name >> newBill.species >> newBill.sellPrice >> newBill.quantity
-        >> newBill.price >> newBill.profit >> newBill.measure;
-    bills.push_back(newBill);
-    cout << "新的销售记录已添加。" << endl;
+void Bills::newLog(){
+
 }
+
+
+
